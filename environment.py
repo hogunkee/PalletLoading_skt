00fce00b37802a8f1f3ccacbed3ad702c73dd504
Jaewon Lee <jwlee2843@gmail.com>
2023-10-08 14:21:47 +0900

    [style] format

--- environment.py
+++ environment.py
@@ -1,17 +1,19 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 class RLEnv(object):
-    def __init__(self, 
-                 resolution=512,
-                 num_steps=100,
-                 num_preview=5,
-                 box_norm=False,
-                 action_norm=False,
-                 render=False,
-                 block_size_min=0.2,
-                 block_size_max=0.4
-                 ):
+    def __init__(
+        self,
+        resolution=512,
+        num_steps=100,
+        num_preview=5,
+        box_norm=False,
+        action_norm=False,
+        render=False,
+        block_size_min=0.2,
+        block_size_max=0.4,
+    ):
         """
         resolution: int (default: 512)
             image resolution of the palette.
@@ -41,20 +43,20 @@ class RLEnv(object):
         self.block_size_max = block_size_max
 
         if self.render:
-            plot0 = plt.subplot2grid((2,3), (0,0))
-            plot1 = plt.subplot2grid((2,3), (0,1))
-            plot2 = plt.subplot2grid((2,3), (0,2))
-            plot3 = plt.subplot2grid((2,3), (1,0), colspan=3)
-            plot0.set_title('previous state')
-            plot1.set_title('current state')
-            plot2.set_title('next block')
-            plot3.set_title('next blocks')
+            plot0 = plt.subplot2grid((2, 3), (0, 0))
+            plot1 = plt.subplot2grid((2, 3), (0, 1))
+            plot2 = plt.subplot2grid((2, 3), (0, 2))
+            plot3 = plt.subplot2grid((2, 3), (1, 0), colspan=3)
+            plot0.set_title("previous state")
+            plot1.set_title("current state")
+            plot2.set_title("next block")
+            plot3.set_title("next blocks")
             self.plots = [plot0, plot1, plot2, plot3]
             for i, p in enumerate(self.plots):
                 p.set_xticks([])
                 p.set_yticks([])
-                if i==0 or i==1:
-                    p.axis('off')
+                if i == 0 or i == 1:
+                    p.axis("off")
             plt.show(block=False)
 
     def reset(self):
@@ -66,56 +68,62 @@ class RLEnv(object):
 
     def render_current_state(self, previous_state=None, box=None):
         pad = int(0.1 * self.resolution)
-        state_pad = np.ones([int(1.2*self.resolution), int(1.2*self.resolution), 3]) * 0.7
+        state_pad = (
+            np.ones([int(1.2 * self.resolution), int(1.2 * self.resolution), 3]) * 0.7
+        )
         pad_mask = np.ones(state_pad.shape[:2])
         pad_mask[pad:-pad, pad:-pad] = 0
 
         pre_state_pad = state_pad.copy()
-        y, x = np.where(previous_state==0)
+        y, x = np.where(previous_state == 0)
         pre_state_pad[y + pad, x + pad] = (1, 1, 1)
-        y, x = np.where(previous_state==1)
+        y, x = np.where(previous_state == 1)
         pre_state_pad[y + pad, x + pad] = (0, 0, 0)
-        y, x = np.where(previous_state==2)
+        y, x = np.where(previous_state == 2)
         pre_state_pad[y + pad, x + pad] = (1, 0, 0)
-        y, x = np.where(np.all(pre_state_pad!=[0.7, 0.7, 0.7], axis=-1) & (pad_mask==1))
+        y, x = np.where(
+            np.all(pre_state_pad != [0.7, 0.7, 0.7], axis=-1) & (pad_mask == 1)
+        )
         pre_state_pad[y, x] = (1, 0, 0)
         self.plots[0].imshow(pre_state_pad)
 
-        state_pad = np.ones([int(1.2*self.resolution), int(1.2*self.resolution), 3]) * 0.7
-        y, x = np.where(self.state==0)
+        state_pad = (
+            np.ones([int(1.2 * self.resolution), int(1.2 * self.resolution), 3]) * 0.7
+        )
+        y, x = np.where(self.state == 0)
         state_pad[y + pad, x + pad] = (1, 1, 1)
-        y, x = np.where(self.state==1)
+        y, x = np.where(self.state == 1)
         state_pad[y + pad, x + pad] = (0, 0, 0)
         if box is not None:
             min_y, max_y, min_x, max_x = np.array(box) + pad
-            state_pad[min_y: max_y, min_x: max_x] = (0, 0, 1)
-        y, x = np.where(self.state==2)
+            state_pad[min_y:max_y, min_x:max_x] = (0, 0, 1)
+        y, x = np.where(self.state == 2)
 
         state_pad[y + pad, x + pad] = (1, 0, 0)
-        y, x = np.where(np.all(state_pad!=[0.7, 0.7, 0.7], axis=-1) & (pad_mask==1))
+        y, x = np.where(np.all(state_pad != [0.7, 0.7, 0.7], axis=-1) & (pad_mask == 1))
         state_pad[y, x] = (1, 0, 0)
         self.plots[1].imshow(state_pad)
 
         block_figures = None
-        if env.step_count==0:
+        if env.step_count == 0:
             next_blocks = [self.next_block] + self.block_que[:-1]
             print(next_blocks)
         else:
             next_blocks = self.block_que
-        #for i, b in enumerate([self.next_block] + self.block_que[:-1]):
+        # for i, b in enumerate([self.next_block] + self.block_que[:-1]):
         for i, b in enumerate(next_blocks):
-            cy, cx = int(0.6*self.resolution), int(0.6*self.resolution)
+            cy, cx = int(0.6 * self.resolution), int(0.6 * self.resolution)
             b = np.round(np.array(b) * self.resolution).astype(int)
             by, bx = b
-            min_y = np.round(cy - (by-1e-5)/2).astype(int)
-            min_x = np.round(cx - (bx-1e-5)/2).astype(int)
-            max_y = np.round(cy + (by-1e-5)/2).astype(int)
-            max_x = np.round(cx + (bx-1e-5)/2).astype(int)
+            min_y = np.round(cy - (by - 1e-5) / 2).astype(int)
+            min_x = np.round(cx - (bx - 1e-5) / 2).astype(int)
+            max_y = np.round(cy + (by - 1e-5) / 2).astype(int)
+            max_x = np.round(cx + (bx - 1e-5) / 2).astype(int)
 
             block_fig = np.ones_like(state_pad)
-            block_fig[min_y: max_y, min_x: max_x] = [0, 0, 0]
+            block_fig[min_y:max_y, min_x:max_x] = [0, 0, 0]
 
-            if i==0:
+            if i == 0:
                 self.plots[2].imshow(block_fig)
             else:
                 if block_figures is None:
@@ -137,7 +145,9 @@ class RLEnv(object):
         self.step_count = 0
 
         # next block: (height, width)
-        self.block_que = np.random.uniform(self.block_size_min, self.block_size_max, [self.num_preview, 2]).tolist()
+        self.block_que = np.random.uniform(
+            self.block_size_min, self.block_size_max, [self.num_preview, 2]
+        ).tolist()
 
         next_block = self.get_next_block()
         return state, next_block
@@ -158,12 +168,12 @@ class RLEnv(object):
 
         # make box region #
         cy, cx = action
-        #by, bx = self.next_block
+        # by, bx = self.next_block
         by, bx = np.round(np.array(self.next_block) * self.resolution).astype(int)
-        min_y = np.round(cy - (by-1e-5)/2).astype(int)
-        min_x = np.round(cx - (bx-1e-5)/2).astype(int)
-        max_y = np.round(cy + (by-1e-5)/2).astype(int)
-        max_x = np.round(cx + (bx-1e-5)/2).astype(int)
+        min_y = np.round(cy - (by - 1e-5) / 2).astype(int)
+        min_x = np.round(cx - (bx - 1e-5) / 2).astype(int)
+        max_y = np.round(cy + (by - 1e-5) / 2).astype(int)
+        max_x = np.round(cx + (bx - 1e-5) / 2).astype(int)
 
         # check out of range #
         if min_y < 0 or min_x < 0:
@@ -176,9 +186,9 @@ class RLEnv(object):
 
         # check collision #
         box_placed = np.zeros([self.resolution, self.resolution])
-        box_placed[min_y: max_y, min_x: max_x] = 1
+        box_placed[min_y:max_y, min_x:max_x] = 1
         self.state = self.state + box_placed
-        if len(np.where(self.state>1)[0]) > 0:
+        if len(np.where(self.state > 1)[0]) > 0:
             collision = True
             reward = 0.0
             episode_end = True
@@ -195,28 +205,37 @@ class RLEnv(object):
         if self.box_norm:
             next_block = self.next_block
         else:
-            next_block = np.round(np.array(self.next_block) * self.resolution).astype(int)
+            next_block = np.round(np.array(self.next_block) * self.resolution).astype(
+                int
+            )
 
         return self.state, next_block, reward, episode_end
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     box_norm = True
     action_norm = True
-    env = RLEnv(box_norm=box_norm, action_norm=action_norm, render=True, block_size_min=0.1, block_size_max=0.25)
+    env = RLEnv(
+        box_norm=box_norm,
+        action_norm=action_norm,
+        render=True,
+        block_size_min=0.1,
+        block_size_max=0.25,
+    )
     state, next_block = env.reset()
 
-    print('Episode starts.')
+    print("Episode starts.")
     for i in range(100):
-        print('step %d.' %i)
+        print("step %d." % i)
         if action_norm:
             random_action = np.random.uniform(0.1, 0.9, 2)
             action = random_action.tolist()
         else:
             random_action = np.random.uniform(0.1, 0.9, 2) * env.resolution
             action = np.round(random_action).astype(int).tolist()
-        print('action:', action)
+        print("action:", action)
         state, next_block, reward, end = env.step(action)
-        print('reward:', reward)
+        print("reward:", reward)
         if end:
-            print('Episode ends.')
+            print("Episode ends.")
             break
