82df71a358cd36a2da2e81b26415d5a55504e93f
hogun-mac <hogunhogun@naver.com>
2023-10-09 21:09:36 +0900

    environment value-1 for boundaries

--- environment.py
+++ environment.py
@@ -139,7 +139,8 @@ class Floor1(object):
         return next_block
 
     def init_scenario(self):
-        state = np.zeros([self.resolution, self.resolution])
+        state = np.zeros([self.resolution-2, self.resolution-2])
+        state = np.pad(state, (1, 1), 'constant', constant_values=(1))
         self.step_count = 0
 
         # next block: (height, width)
