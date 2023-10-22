a250e72ca1834c4e719ed7daa5171332d4bfeaee
Jaewon Lee <jwlee2843@gmail.com>
2023-10-22 17:52:42 +0900

    [feat] rotate blocks while searching

--- greedy_baseline.py
+++ greedy_baseline.py
@@ -9,37 +9,50 @@ class RulebasePallasdfetLoader:
         self.obs_resolution = obs_resolution
         self.margin_ratio = margin_ratio
 
+    def check_if_block_fits(self, image_obs, pos_index, block_dimension_in_pixel):
+        # Time complexity of this method can be reduced to O(1) from current O(n^2) using memory!
+        # Return true if give block fits in given position(index) of the pallet
+
+        # position index of intrerest in observation
+        obs_i, obs_j = pos_index
+        block_pixel_width, block_pixel_height = block_dimension_in_pixel
+
+        # check out of bound
+        if (
+            obs_i + block_pixel_width > self.obs_resolution
+            or obs_j + block_pixel_height > self.obs_resolution
+        ):
+            return False
+        # check collision
+        for block_pix in range(block_pixel_width * block_pixel_height):
+            d_i = block_pix // block_pixel_height
+            d_j = block_pix % block_pixel_height
+            if image_obs[obs_i + d_i][obs_j + d_j] == 1:
+                return False
+        return True
+
     def greedy_search(self, image_obs, block_size):
-        block_res_width = math.ceil(block_size[0] * self.obs_resolution)
-        block_res_height = math.ceil(block_size[1] * self.obs_resolution)
+        block_pixel_width = math.ceil(block_size[0] * self.obs_resolution)
+        block_pixel_height = math.ceil(block_size[1] * self.obs_resolution)
         for elem in range(self.obs_resolution * self.obs_resolution):
-            obs_i = elem // self.obs_resolution
-            obs_j = elem % self.obs_resolution
+            pos_idx = (elem // self.obs_resolution, elem % self.obs_resolution)
+            block_pixel_dim = (block_pixel_width, block_pixel_height)
+            block_pixel_dim_rot = (block_pixel_height, block_pixel_width)
 
-            # check out of bound
-            block_out_of_index = False
-            if (
-                obs_i + block_res_width > self.obs_resolution
-                or obs_j + block_res_height > self.obs_resolution
-            ):
-                block_out_of_index = True
-
-            if block_out_of_index:
-                continue
-            # check collision
-            block_collision = False
-            for block_pix in range(block_res_width * block_res_height):
-                d_i = block_pix // block_res_height
-                d_j = block_pix % block_res_height
-                if image_obs[obs_i + d_i][obs_j + d_j] == 1:
-                    block_collision = True
-                    break
-            if not block_collision:
+            if self.check_if_block_fits(image_obs, pos_idx, block_pixel_dim):
                 # found valid action!
-                action_i = (obs_i + 0.5 * block_res_width) / self.obs_resolution
-                action_j = (obs_j + 0.5 * block_res_height) / self.obs_resolution
-                return [action_i, action_j]
-
+                action_i = (pos_idx[0] + 0.5 * block_pixel_dim[0]) / self.obs_resolution
+                action_j = (pos_idx[1] + 0.5 * block_pixel_dim[1]) / self.obs_resolution
+                return [0, action_i, action_j]
+            elif self.check_if_block_fits(image_obs, pos_idx, block_pixel_dim_rot):
+                # found valid action!
+                action_i = (
+                    pos_idx[0] + 0.5 * block_pixel_dim_rot[0]
+                ) / self.obs_resolution
+                action_j = (
+                    pos_idx[1] + 0.5 * block_pixel_dim_rot[1]
+                ) / self.obs_resolution
+                return [1, action_i, action_j]
         return None
 
     def rotate_block(self, block_size):
@@ -48,15 +61,10 @@ class RulebasePallasdfetLoader:
 
     def get_greedy_action(self, obs):
         image_obs, block_size = obs
-        action_pos = self.greedy_search(image_obs, block_size)
-        action_rot = [0]
-        if action_pos == None:
-            action_pos = self.greedy_search(image_obs, self.rotate_block(block_size))
-            action_rot = [1]
-
-        if action_pos == None:
+        action = self.greedy_search(image_obs, block_size)
+        if action == None:
             return [0, 0, 0]
-        return action_rot + action_pos
+        return action
 
 
 if __name__ == "__main__":
@@ -72,7 +80,7 @@ if __name__ == "__main__":
     predictor = RulebasePallasdfetLoader(resolution)
 
     total_reward = 0.0
-    num_episodes = 100
+    num_episodes = 1000
     for ep in range(num_episodes):
         obs = env.reset()
         ep_reward = 0.0
---
+++ rulebase_baseline.py
@@ -0,0 +1,90 @@
+import random
+import math
+import numpy as np
+from environment import Floor1
+
+
+class RulebasePallasdfetLoader:
+    def __init__(self, obs_resolution, margin_ratio=0.05):
+        self.obs_resolution = obs_resolution
+        self.margin_ratio = margin_ratio
+
+    def greedy_search(self, image_obs, block_size):
+        block_res_width = math.ceil(block_size[0] * self.obs_resolution)
+        block_res_height = math.ceil(block_size[1] * self.obs_resolution)
+        for elem in range(self.obs_resolution * self.obs_resolution):
+            obs_i = elem // self.obs_resolution
+            obs_j = elem % self.obs_resolution
+
+            # check out of bound
+            block_out_of_index = False
+            if (
+                obs_i + block_res_width > self.obs_resolution
+                or obs_j + block_res_height > self.obs_resolution
+            ):
+                block_out_of_index = True
+
+            if block_out_of_index:
+                continue
+            # check collision
+            block_collision = False
+            for block_pix in range(block_res_width * block_res_height):
+                d_i = block_pix // block_res_height
+                d_j = block_pix % block_res_height
+                if image_obs[obs_i + d_i][obs_j + d_j] == 1:
+                    block_collision = True
+                    break
+            if not block_collision:
+                # found valid action!
+                action_i = (obs_i + 0.5 * block_res_width) / self.obs_resolution
+                action_j = (obs_j + 0.5 * block_res_height) / self.obs_resolution
+                return [action_i, action_j]
+
+        return None
+
+    def rotate_block(self, block_size):
+        rotated_block = [block_size[1], block_size[0]]
+        return rotated_block
+
+    def get_greedy_action(self, obs):
+        image_obs, block_size = obs
+        action_pos = self.greedy_search(image_obs, block_size)
+        action_rot = [0]
+        if action_pos == None:
+            action_pos = self.greedy_search(image_obs, self.rotate_block(block_size))
+            action_rot = [1]
+
+        if action_pos == None:
+            return [0, 0, 0]
+        return action_rot + action_pos
+
+
+if __name__ == "__main__":
+    box_norm = True
+    resolution = 10
+    env = Floor1(
+        resolution=resolution,
+        box_norm=box_norm,
+        action_norm=True,
+        render=False,
+        discrete_block=True,
+    )
+    predictor = RulebasePallasdfetLoader(resolution)
+
+    total_reward = 0.0
+    num_episodes = 100
+    for ep in range(num_episodes):
+        obs = env.reset()
+        ep_reward = 0.0
+        # print(f'Episode {ep} starts.')
+        for i in range(100):
+            action = predictor.get_greedy_action(obs)
+            obs, reward, end = env.step(action)
+            ep_reward += reward
+            if end:
+                # print('Episode ends.')
+                break
+        # print("    ep_reward: ", ep_reward)
+        total_reward += ep_reward
+    avg_score = total_reward / num_episodes
+    print("average score: ", avg_score)
